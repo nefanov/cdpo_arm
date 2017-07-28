a2a2ee6bd4700e0c79bfc86cdd0b43a829383fb0
636task <efanov.nn@mipt.ru>
2017-07-28 22:36:32 +0300

    More laconically var names

--- task_1/3_libc_io.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	scanf("%d%d",&a,&b);
-	printf("Output:%d,%d\n",a,b);
-
-	return 0;
-}
--- task_2/10_simd_neon_vect_add.c
+++
@@ -1,7 +0,0 @@
-void v_a(int * __restrict dst, int *__restrict  src,
-		 unsigned depth, int v) {
-	unsigned i;
-	for (i=0; i < (depth & ~3); i++ )
-		result = source[i] + val;
-	return;
-}
